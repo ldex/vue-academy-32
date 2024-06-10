72e1c8383afae673d8ccdc4eccf07cad92d975be
ldex <laurent.duveau+git@gmail.com>
2024-06-10 16:01:22 -0400

    Fin du jour 1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import ProductList from '@/components/ProductList.vue'
+import ProductService from '@/services/ProductService.js'
+
+let products = ref([])
+const errorMessage = ref(null)
+const isLoading = ref(false)
+
+isLoading.value = true
+ProductService.getProducts()
+  .then(data => products.value = data)
+  .catch(error => {
+    errorMessage.value = 'There was an error getting products from server, ' + error
+  })
+  .finally(() => isLoading.value = false)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <h1>Vue Store</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/products">Products</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+    <hr />
+    <footer>Copyright Vue Academy 2024</footer>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
